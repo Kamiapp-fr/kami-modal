0365dd6b9f27d647cfeafdd446742e2d1902d116
Remy Bouvard <remy.bouvard@epitech.eu>
2020-05-15 09:23:02 +0200

    :tada: initial commit but with the real name of files

--- rollup.config.ts
+++ rollup.config.ts
@@ -7,7 +7,7 @@ import json from 'rollup-plugin-json'
 
 const pkg = require('./package.json')
 
-const libraryName = 'kami-flash'
+const libraryName = 'kami-modal'
 
 export default {
   input: `src/${libraryName}.ts`,
